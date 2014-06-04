7ecdfe11cca37738a1542c3afc5a0e90b1ac222c
Olivia Simpson <oliviamsimpson@gmail.com>
2014-06-04 14:36:06 -0700

    callable function now, not a script

--- compute_K.py
+++ compute_K.py
@@ -1,10 +1,16 @@
 import math
-import sys
 
-t = float(sys.argv[1])
-eps = float(sys.argv[2])
+def compute_K_noT(eps):
+    num = math.log(1/eps)
+    den = math.log(num)
+    return num/den
 
-num = t*(2+math.log(t)) + math.log(2/eps)
-den = math.log(num)
+def compute_K(t, eps):
+    num = t*(2+math.log(t)) + math.log(2/eps)
+    den = math.log(num)
+    return num/den
 
-print num/den
+def compute_K_new(t, eps):
+    num = t*math.log(t) + math.log(2/eps)
+    den = math.log(num)
+    return num/den
