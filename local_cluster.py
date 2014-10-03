93b63de7dfe35f094b329fdadebbb930a1113317
Olivia Simpson <oliviamsimpson@gmail.com>
2014-10-03 15:56:58 -0700

    testing against matrix multiply

--- local_cluster.py
+++ local_cluster.py
@@ -2,16 +2,48 @@ import Network
 import networkx as nx
 import numpy as np
 import pydot
+import math
 
-def approx_hkpr_multiply(Net, t, seed_vec=None, trials=100):
+
+def approx_hkpr_matmult(Net, t, seed_vec=None, trials=100, verbose=False):
 	k = np.random.poisson(lam=t)
+	if verbose: print 'k', k, 'random walk steps'
 	appr = np.dot(seed_vec, np.linalg.matrix_power(Net.walk_mat, k))
 	for r in range(trials-1):
 		k = np.random.poisson(lam=t)
+		if verbose: print 'k', k, 'random walk steps'
 		appr += np.dot(seed_vec, np.linalg.matrix_power(Net.walk_mat, k))
 	appr = appr/trials
+	appr = np.array(appr)[0]
 
 	return appr
+
+def approx_hkpr_rwk(Net, k, seed_vec=None, eps=0.01,
+                verbose=False):
+    '''
+    Outputs an eps-approximate heat kernel pagerank vector computed
+    with random walks.
+    '''
+    # initialize 0-vector of size n
+    n = Net.size
+    approxhkpr = np.zeros(n) 
+
+    r = (16.0/eps**3)*math.log(n)
+    # r = (16.0/eps)*math.log(n)
+
+    if verbose:
+        print 'r: ', r
+        print 'k: ', k
+
+    for iter in range(int(r)):
+        # k = np.random.poisson(lam=t)
+        # k = int(min(k,K))
+
+        v = Net.random_walk(k, seed_vec=seed_vec, verbose=False)
+        
+        approxhkpr[Net.node_to_index[v]] += 1
+
+    return approxhkpr/r
 
 
 def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
@@ -21,10 +53,11 @@ def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
     with random walks.
     '''
     # initialize 0-vector of size n
-    approxhkpr = np.zeros(Net.size) 
+    n = Net.size
+    approxhkpr = np.zeros(n) 
 
-    # r = (16.0/eps**3)*math.log(n)
-    r = (16.0/eps)*math.log(n)
+    r = (16.0/eps**3)*math.log(n)
+    # r = (16.0/eps)*math.log(n)
     K = (math.log(1.0/eps))/(math.log(math.log(1.0/eps)))
     # K = t
 
@@ -34,10 +67,22 @@ def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
 
     for iter in range(int(r)):
         k = np.random.poisson(lam=t)
-        k = int(min(k,K))
+        # k = int(min(k,K))
 
         v = Net.random_walk(k, seed_vec=seed_vec, verbose=False)
         
         approxhkpr[Net.node_to_index[v]] += 1
 
     return approxhkpr/r
+
+
+def approx_hkpr_err(true, appr, eps):
+	if true.size != appr.size:
+		print 'vector dimensions do not match'
+		return
+	err = 0
+	for i in range(true.size):
+		comp_err = (abs(true[i]-appr[i])) - (eps*true[i])
+		if comp_err > 0:
+			err += comp_err
+	return err
