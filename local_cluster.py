c9135cb07248f6a3c67624a937f9c7ee0505f0ad
Olivia Simpson <oliviamsimpson@gmail.com>
2014-10-03 16:53:46 -0700

    remove keywork iter dummy

--- local_cluster.py
+++ local_cluster.py
@@ -5,49 +5,48 @@ import pydot
 import math
 
 
-def approx_hkpr_matmult(Net, t, seed_vec=None, trials=100, verbose=False):
-	k = np.random.poisson(lam=t)
-	if verbose: print 'k', k, 'random walk steps'
-	appr = np.dot(seed_vec, np.linalg.matrix_power(Net.walk_mat, k))
-	for r in range(trials-1):
-		k = np.random.poisson(lam=t)
-		if verbose: print 'k', k, 'random walk steps'
-		appr += np.dot(seed_vec, np.linalg.matrix_power(Net.walk_mat, k))
-	appr = appr/trials
-	appr = np.array(appr)[0]
+def approx_hkpr_matmult(Net, t, seed_vec=None, eps=0.01, verbose=False):
+    """
+    Use matrix multiplication to estimate a heat kernel pagerank vector as an
+    expected distribution of random walks
+    """
+    r = (16.0/eps)*math.log(n)
+    k = np.random.poisson(lam=t)
+    if verbose: print 'k', k, 'random walk steps'
+    appr = np.dot(seed_vec, np.linalg.matrix_power(Net.walk_mat, k))
+    for i in range(r-1):
+        k = np.random.poisson(lam=t)
+        if verbose: print 'k', k, 'random walk steps'
+        appr += np.dot(seed_vec, np.linalg.matrix_power(Net.walk_mat, k))
+    appr = appr/r
+    appr = np.array(appr)[0]
 
-	return appr
+    return appr
 
-def approx_hkpr_rwk(Net, k, seed_vec=None, eps=0.01,
-                verbose=False):
-    '''
-    Outputs an eps-approximate heat kernel pagerank vector computed
-    with random walks.
-    '''
+def approx_hkpr_rwk(Net, k, seed_vec=None, eps=0.01, verbose=False):
+    """
+    Tool for checking approximating fP^k with random walks of length k
+    """
     # initialize 0-vector of size n
     n = Net.size
     approxhkpr = np.zeros(n) 
 
-    r = (16.0/eps**3)*math.log(n)
+    # r = (16.0/eps**3)*math.log(n)
+    r = (16.0/eps**2)*math.log(n)
     # r = (16.0/eps)*math.log(n)
 
     if verbose:
         print 'r: ', r
         print 'k: ', k
 
-    for iter in range(int(r)):
-        # k = np.random.poisson(lam=t)
-        # k = int(min(k,K))
-
+    for i in range(int(r)):
         v = Net.random_walk(k, seed_vec=seed_vec, verbose=False)
-        
         approxhkpr[Net.node_to_index[v]] += 1
 
     return approxhkpr/r
 
 
-def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
-                verbose=False):
+def approx_hkpr(Net, t, seed_vec=None, eps=0.01, verbose=False):
     '''
     Outputs an eps-approximate heat kernel pagerank vector computed
     with random walks.
@@ -56,7 +55,8 @@ def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
     n = Net.size
     approxhkpr = np.zeros(n) 
 
-    r = (16.0/eps**3)*math.log(n)
+    # r = (16.0/eps**3)*math.log(n)
+    r = (16.0/eps**2)*math.log(n)
     # r = (16.0/eps)*math.log(n)
     K = (math.log(1.0/eps))/(math.log(math.log(1.0/eps)))
     # K = t
@@ -65,7 +65,7 @@ def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
         print 'r: ', r
         print 'K: ', K
 
-    for iter in range(int(r)):
+    for i in range(int(r)):
         k = np.random.poisson(lam=t)
         # k = int(min(k,K))
 
@@ -77,12 +77,12 @@ def approx_hkpr(Net, t, seed_vec=None, eps=0.01,
 
 
 def approx_hkpr_err(true, appr, eps):
-	if true.size != appr.size:
-		print 'vector dimensions do not match'
-		return
-	err = 0
-	for i in range(true.size):
-		comp_err = (abs(true[i]-appr[i])) - (eps*true[i])
-		if comp_err > 0:
-			err += comp_err
-	return err
+    if true.size != appr.size:
+        print 'vector dimensions do not match'
+        return
+    err = 0
+    for i in range(true.size):
+        comp_err = (abs(true[i]-appr[i])) - (eps*true[i])
+        if comp_err > 0:
+            err += comp_err
+    return err
