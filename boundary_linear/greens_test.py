dbb93cc20e2aecf3375858bfffe8ea42d7a6bd8a
Olivia Simpson <oliviamsimpson@gmail.com>
2014-11-03 21:19:55 -0800

    testing with hkpr sum not sampling

--- boundary_linear/greens_test.py
+++ boundary_linear/greens_test.py
@@ -22,6 +22,8 @@ def test_network(Net, eps=0.01, randfactor=1):
     xS_rie = solver.restricted_solution_riemann(Net, boundary_vec, subset, eps=eps)
     print 'computing restricted solution by sampling the Riemann sum...'
     xS_rieSample = solver.restricted_solution_riemann_sample(Net, boundary_vec, subset, eps=eps)
+    print 'computing restricted solution by summing heat kernel pagerank vectors...'
+    xS_hkpr = solver.greens_solver_exphkpr_riemann(Net, boundary_vec, subset, eps=eps)
     print 'computing restricted solution by sampling heat kernel pagerank...'
     xS_hkprSample = solver.greens_solver_exphkpr(Net, boundary_vec, subset, eps=eps)
     b1 = solver.compute_b1(Net, boundary_vec, subset)
@@ -34,6 +36,11 @@ def test_network(Net, eps=0.01, randfactor=1):
     allowable_err_sample = eps*( np.linalg.norm(b1) + np.linalg.norm(xS_true) + np.linalg.norm(xS_rie) )
     f.write(str(max(0, np.linalg.norm(xS_true-xS_rieSample) - allowable_err_sample)))
 
+    f.write('\nerror in HKPR summing:\t')
+    allowable_err = eps*(np.linalg.norm(b1)+np.linalg.norm(xS_true))
+    f.write(str(max(0, np.linalg.norm(xS_true-xS_hkpr) - allowable_err)))
+
+
     f.write('\nerror in HKPR sampling:\t')
     f.write(str(max(0, np.linalg.norm(xS_true-xS_hkprSample) - allowable_err_sample)))
 
@@ -42,7 +49,7 @@ def test_network(Net, eps=0.01, randfactor=1):
 # test_network(example)
 # f.close()
 
-f = open('sampling_tests_dolphins.txt', 'w')
+f = open('sampling_tests_dolphins3.txt', 'w')
 print '\ngenerating dolphins network...'
 dolphins = Network(GRAPH_DATASETS['dolphins'])
 f.write('\n\ndolphins\n')
@@ -51,7 +58,7 @@ test_network(dolphins, randfactor=10)
 test_network(dolphins, randfactor=100)
 f.close()
 
-f = open('sampling_tests_lesmis.txt', 'w')
+f = open('sampling_tests_lesmis3.txt', 'w')
 print '\ngenerating lesmis network...'
 lesmis = Network(GRAPH_DATASETS['lesmis'])
 f.write('\n\nlesmis\n')
@@ -60,7 +67,7 @@ test_network(lesmis, randfactor=10)
 test_network(lesmis, randfactor=100)
 f.close()
 
-f = open('sampling_tests_power.txt', 'w')
+f = open('sampling_tests_power3.txt', 'w')
 print '\ngenerating power network...'
 power = Network(GRAPH_DATASETS['power'])
 f.write('\n\npower\n')
@@ -69,7 +76,7 @@ test_network(power, randfactor=10)
 test_network(power, randfactor=100)
 f.close()
 
-f = open('sampling_tests_facebook.txt', 'w')
+f = open('sampling_tests_facebook3.txt', 'w')
 print '\ngenerating facebook network...'
 facebook = Network(edge_list=GRAPH_DATASETS['facebook'])
 f.write('\n\nfacebook\n')
