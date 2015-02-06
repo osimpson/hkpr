18dc9e236189ed2993cb1b1c4196682f3c6c122c
Olivia Simpson <oliviamsimpson@gmail.com>
2015-02-06 13:54:22 -0800

    localcluster method that avoids computing exp_hkpr

--- local_cluster/local_cluster.py
+++ local_cluster/local_cluster.py
@@ -214,8 +214,8 @@ def approx_hkpr_seed_mp_dict(Net, t, start_node, K=None, eps=0.01, verbose=False
     """
     n = Net.size
 
-    r = (16.0/eps**3)*np.log(n)
-    # r = (16.0/eps**2)*np.log(n)
+    # r = (16.0/eps**3)*np.log(n)
+    r = (16.0/eps**2)*np.log(n)
     # r = (16.0/eps)*np.log(n)
     # r = (16.0/eps**2)*np.log(100)
 
@@ -474,7 +474,80 @@ def local_cluster_hkpr_mincheeg(Net, start_node, target_size=None,
         sweep_set.append(rank[j])
         # vol_ach = Net.volume(subset=sweep_set) #volume of sweep set
         vol_ach += Net.graph.degree(rank[j])
-        if vol_ach > 2*target_vol:
+        # if vol_ach > 2*target_vol:
+        if vol_ach > 0.5*Net.volume():
+           break
+        # cheeg_ach = Net.cheeger_ratio(sweep_set) #cheeger ratio of sweep set
+        #calculate edge boundary
+        for nb in Net.graph.neighbors(rank[j]):
+            if nb in sweep_set:
+                edge_bound_ach = edge_bound_ach - 1
+            elif nb not in sweep_set:
+                edge_bound_ach += 1
+        cheeg_ach = float(edge_bound_ach)/vol_ach
+        if cheeg_ach < min_cheeg:
+            min_sweep = sweep_set
+            best_vol = vol_ach
+            min_cheeg = cheeg_ach
+
+    sweep_heat_vals = {}
+    for nd in Net.graph.nodes():
+        if nd in min_sweep:
+            sweep_heat_vals[nd] = dn_heat_val[nd]
+        else:
+            sweep_heat_vals[nd] = 0
+
+    return min_sweep, sweep_heat_vals, best_vol, min_cheeg
+
+
+def local_cluster_hkpr_mincheeg_vec(Net, dn_heat_val, target_size=None,
+                                    target_vol=None, target_cheeg=None,
+                                    eps=0.01, verbose=False):
+    """
+    Compute the best cluster in terms of Cheeger ratio around a seed node,
+    given a vector.
+
+    Parameters:
+        dn_heat_val, the dictionary of vector values used for the sweep.
+        target_size, the desired size of the output cluster.  If not specified,
+            set to n/2
+        target_vol, the desired volume of the output cluster.  If not specified,
+            set to vol(G)/4
+        target_cheeg, the desired Cheeger ratio of the output cluster.  If not
+            specified, set to 1/3.
+        eps, the desired error parameter
+
+    Output:
+        1. a dictionary of cluster nodes and normalized heat kernel values
+        2. the volume of the cluster
+        3. the Cheeger ratio of the set
+    """
+    if target_size is None:
+        target_size = Net.size/2.
+    if target_vol is None:
+        target_vol = Net.volume()/4.
+    if target_cheeg is None:
+        target_cheeg = 1./3
+    t = (1./target_cheeg)*np.log( (2*np.sqrt(target_vol))/(1-eps) + 2*eps*target_size )
+
+    print 'performing a sweep...'
+    #node ranking (this is a list of nodes!)
+    rank = sorted(dn_heat_val, key=dn_heat_val.get, reverse=True)
+
+    #perform a sweep
+    sweep_set = []
+    vol_ach = 0.0
+    edge_bound_ach = 0.0
+    cheeg_ach = 0.0
+    min_sweep = []
+    best_vol = 0.0
+    min_cheeg = 1.0
+    for j in range(len(rank)):
+        sweep_set.append(rank[j])
+        # vol_ach = Net.volume(subset=sweep_set) #volume of sweep set
+        vol_ach += Net.graph.degree(rank[j])
+        # if vol_ach > 2*target_vol:
+        if vol_ach > 0.5*Net.volume():
            break
         # cheeg_ach = Net.cheeger_ratio(sweep_set) #cheeger ratio of sweep set
         #calculate edge boundary
@@ -531,9 +604,9 @@ def local_cluster_pr(Net, start_node, target_cheeg=None):
     cheeg_ach = 0.0
     for j in range(len(rank)):
         sweep_set.append(rank[j])
-        # vol_ach = Net.volume(subset=sweep_set) #volume of sweep set
+    # vol_ach = Net.volume(subset=sweep_set) #volume of sweep set
 	vol_ach += Net.graph.degree(rank[j])
-        #if vol_ach > (2./3)*Net.volume():
+    #if vol_ach > (2./3)*Net.volume():
 	if vol_ach > 0.5*Net.volume():
             print 'NO CUT FOUND'
             return
